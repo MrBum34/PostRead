ac78115d4f5a59e22a9924fec2f639f5c757e6af
Ustinov_Egor <egor.ustinov.34@gmail.com>
2023-04-28 19:53:49 +0300

    Рефакторинг

--- src/main/java/by/vstu/postread/controller/UserController.java
+++ src/main/java/by/vstu/postread/controller/UserController.java
@@ -3,9 +3,9 @@ package by.vstu.postread.controller;
 import by.vstu.postread.entity.User;
 import by.vstu.postread.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -19,12 +19,9 @@ public class UserController {
 
     @PostMapping(value = "/register")
     public String postRegister(User user) {
-//        User user=new User();
-//        user.setUsername(username);
-//        user.setPassword(password);
         System.out.println(user.toString());
         boolean is = userService.register(user);
-        if (is){
+        if (is) {
             return "redirect:/login";
         }
         return "redirect:/register";
